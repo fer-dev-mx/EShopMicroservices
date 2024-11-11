80558a487168dc8911baaeb037a50a91547d580e
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2024-11-11 02:35:22 -0600

    Add CQRS interfaces and update project references
    Added comments and documentation to `ICommand.cs` and `IQuery.cs` to explain their roles in the CQRS pattern. Introduced `ICommandHandler.cs` and `IQueryHandler.cs` with interfaces for handling commands and queries, including detailed documentation. Removed the `MediatR` package reference from `Catalog.API.csproj` and added a project reference to `BuildingBlocks.csproj`.

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -2,10 +2,22 @@
 
 namespace BuildingBlocks.CQRS;
 
+/*
+    Used to create command abstractions within a CQRS pattern
+ */
+
+/// <summary>
+/// Essentially, this non-generic ICommand interface is for commands that don’t need to return a result.
+/// Unit is a type from MediatR (similar to void in other contexts), representing a "no response" scenario.
+/// </summary>
 public interface ICommand : ICommand<Unit>
 {
 }
 
+/// <summary>
+/// Inherits from MediatR’s IRequest<TResponse>
+/// </summary>
+/// <typeparam name="TResponse">In MediatR, IRequest<TResponse> represents a request that will be processed by a handler, which returns a response of type TResponse.</typeparam>
 public interface ICommand<out TResponse> : IRequest<TResponse>
 {
 }
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+/// <summary>
+/// This interface defines a handler for queries (read-only operations) that return a response of type TResponse. By implementing this interface, you create a handler that knows how to process a specific query and return the expected data.
+/// </summary>
+/// <typeparam name="TQuery">Ensures that TQuery is a query that expects a response of type TResponse.</typeparam>
+/// <typeparam name="TResponse"></typeparam>
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
