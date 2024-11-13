98701fb547207dc2e0cb10a649473ab4d89a6999
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2024-11-13 14:33:27 -0600

    Update to .NET 8, integrate Marten for database operations
    Refactored CreateProductHandler.cs to use IDocumentSession for database operations with Marten. Configured Marten in Program.cs with connection string from appsettings.json. Updated appsettings.json with new database connection string.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -20,9 +18,10 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
         };
 
         // Save product entity to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
         // Return CreateProductResult object
-
-
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
