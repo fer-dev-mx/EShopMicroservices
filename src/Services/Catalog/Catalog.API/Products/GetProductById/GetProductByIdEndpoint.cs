e338104e0057416a9273e08a111ec0aa85b54843
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2024-11-22 01:12:58 -0600

    Add GetProductById endpoint and custom exception

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+// public record GetProductsByIdRequest();
+public record GetProductsByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductsByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductsByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get a product by Id");
+    }
+}
