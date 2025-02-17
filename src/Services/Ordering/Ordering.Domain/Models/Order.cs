8732c32a085cab8a28bd6ab8e60e748ac054caa8
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-02-17 01:22:49 -0600

    refactor: use value objects for Order properties
    Replaced primitive types with value objects in Order class.
    Added CustomerId and OrderName value objects.

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -5,8 +5,8 @@ public class Order : Aggregate<Guid>
     private readonly List<OrderItem> _orderItems = new();
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
 
-    public Guid CustomerId { get; private set; } = default!;
-    public string OrderName { get; private set; } = default!;
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
     public Address ShippingAddress { get; private set; } = default!;
     public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
