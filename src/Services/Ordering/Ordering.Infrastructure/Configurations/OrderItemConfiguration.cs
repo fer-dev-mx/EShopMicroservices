d6478c65eda1cfe34111949eb42e9e8cc2e03c48
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-03-01 17:11:17 -0600

    config: Mapping DDD Objects to EF Entities
    Introduced new entity configurations for Customer, Order, OrderItem, and Product entities. Implemented Customer and Product configurations; Order and OrderItem pending.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
