3e4576ad9d3f2f23d9beb57c1799854b4fb63129
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-04-12 16:52:15 -0600

    feat: add health checks for ordering api

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 
@@ -8,6 +10,9 @@ public static class DependencyInjection
     {
         services.AddCarter();
         services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
+        
         return services;
     }
 
@@ -15,6 +20,12 @@ public static class DependencyInjection
     {
         app.MapCarter();
         app.UseExceptionHandler(options => { });
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        
         return app;
     }
 }
