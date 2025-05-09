9e5ab20d040e36f07d7007d585781c66d4776cf5
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2025-05-09 11:57:10 -0600

    feat: Add "Add to Cart" functionality to Index page
    Added IBasketService to IndexModel and registered it in DI.
    Implemented OnPostAddToCartAsync to handle cart updates.
    Enhanced basket handling in IBasketService.

--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddRefitClient<ICatalogService>()
     .ConfigureHttpClient(c => {
         c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
     });
+builder.Services.AddRefitClient<IBasketService>()
+    .ConfigureHttpClient(c => {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
 
 var app = builder.Build();
 
